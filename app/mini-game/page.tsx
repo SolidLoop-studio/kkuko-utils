660f67c176f3f212fe497f69385782d21083a2b8
jtw <jtw7913@gmail.com>
2025-12-20 20:57:25 +0900

    feat: 모바일 환경 미지원 추가

--- app/mini-game/page.tsx
+++ app/mini-game/page.tsx
@@ -1,12 +1,17 @@
 import Providers from "./providers";
 import Game from "./game/Game";
+import MobileUnsupported from "./MobileUnsupported";
 
 const MiniGamePage = () => {
     return (
         <Providers>
-            <div className="flex justify-center mt-8">
+            {/* Game visible on medium and larger screens */}
+            <div className="hidden md:flex justify-center mt-8 mb-16">
                 <Game />
             </div>
+
+            {/* Mobile-only message */}
+            <MobileUnsupported />
         </Providers>
     );
 };
