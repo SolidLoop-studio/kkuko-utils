1935fba2dac8890c93b098bedff0355ab6db69db
jtw <jtw7913@gmail.com>
2026-02-13 23:35:45 +0900

    fix: 분당 옵션 계산 오류 수정

--- app/kkuko/profile/utils/profileHelper.ts
+++ app/kkuko/profile/utils/profileHelper.ts
@@ -48,13 +48,13 @@ export const calculateTotalOptions = (itemsData: ItemInfo[]) => {
             const relevantOptions = Date.now() >= item.options.date ? item.options.after : item.options.before;
             Object.entries(relevantOptions).forEach(([key, value]) => {
                 if (value !== undefined && typeof value === 'number' && !isNaN(value)) {
-                    totals[key] = (totals[key] || 0) + Number(value) * (key[0] === 'g' ? 100 : 1000);
+                    totals[key] = (totals[key] || 0) + Number(value) * (key[0] === 'g' ? 100 : 1);
                 }
             });
         } else {
             Object.entries(item.options).forEach(([key, value]) => {
                 if (value !== undefined && typeof value === 'number' && !isNaN(value)) {
-                    totals[key] = (totals[key] || 0) + Number(value) * (key[0] === 'g' ? 100 : 1000);
+                    totals[key] = (totals[key] || 0) + Number(value) * (key[0] === 'g' ? 100 : 1);
                 }
             });
         }
