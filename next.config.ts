1a16eefcf4665dbf3894d64d11b507204d8d6264
jtw <jtw7913@gmail.com>
2026-08-30 21:54:16 +0900

    chore: 잡파일

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'api.solidloop-studio.xyz',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '54321',
+        pathname: '/**',
       }
     ],
     localPatterns: [
