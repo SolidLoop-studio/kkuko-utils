72d4e8324c2c4dc4b36b37ed51f68dba2cc6919a
jtw <jtw7913@gmail.com>
2026-08-31 20:49:02 +0900

    chore: next config

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: '127.0.0.1',
-        port: '54321',
+        port: '55321',
         pathname: '/**',
       }
     ],
